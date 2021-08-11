dc5462eca71aca5326d9f57de1ca047a4d029253
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-11 01:52:18 -0300

    fix unique project name

--- src/pages/RegisterProject/index.jsx
+++ src/pages/RegisterProject/index.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { toast } from 'react-toastify';
 import { useHistory } from 'react-router-dom';
 import * as S from './styles';
-import { newProject, newActivity, getProjectByName } from '../../services/api';
+import { newProject, newActivity } from '../../services/api';
 import Loading from '../../components/Loading';
 
 const RegisterProject = () => {
@@ -33,8 +33,6 @@ const RegisterProject = () => {
         !regEx.test(endDateActivity)
       )
         return toast.error('Data vazia ou inválida!');
-      const name = await getProjectByName(nameProject);
-      if (name._id) return toast.error('Já existe um projeto com este nome!');
 
       const resp = await newProject(
         nameProject,
