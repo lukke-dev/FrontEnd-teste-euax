04687253e72c2ee727ab2938cecd9a5ab772173b
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-11 01:12:08 -0300

    fix unique project name

--- src/pages/RegisterProject/index.jsx
+++ src/pages/RegisterProject/index.jsx
@@ -33,7 +33,9 @@ const RegisterProject = () => {
       )
         return toast.error('Data vazia ou inválida!');
       const name = await getProjectByName(nameProject);
-      if (!name.data) return toast.error('Já existe um projeto com este nome!');
+      if (!name.data) {
+        return toast.error('Já existe um projeto com este nome!');
+      }
       const resp = await newProject(
         nameProject,
         startDateProject,
