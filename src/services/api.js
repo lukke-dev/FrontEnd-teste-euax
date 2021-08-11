974c695196ffac8fd898f7bef5472967a448faad
Lucas dos Santos <lucas.ffr2014@gmail.com>
2021-08-11 00:57:09 -0300

    fix unique project name

--- src/services/api.js
+++ src/services/api.js
@@ -22,6 +22,9 @@ export const getProjects = async (id) => {
 export const getActivies = async (id) =>
   Axios.get(`${baseUrl}/activies/${id}`).then((resp) => resp.data);
 
+export const getProjectByName = async (name) =>
+  Axios.get(`${baseUrl}/name/${name}`).then((resp) => resp.data);
+
 export const delProject = async (id) => {
   Axios.delete(`${baseUrl}/del/${id}`);
   Axios.delete(`${baseUrl}/activies/project/${id}`);
